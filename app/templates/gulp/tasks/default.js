a172d8486bb6c4b6203b056c5c5a048a07f3cc57
Julien Goux <julien.goux@live.fr>
2014-07-25 11:38:41 +0200

    change fonts task into assets task

--- app/templates/gulp/tasks/default.js
+++ app/templates/gulp/tasks/default.js
@@ -7,14 +7,14 @@ module.exports = gulp.task('default', function() {
     if (release) {
         runSequence(
             'clean',
-            ['index', 'styles', 'images', 'fonts', 'templates', 'lint'],
+            ['index', 'styles', 'images', 'assets', 'templates', 'lint'],
             'browserify',
             ['minify','serve']
         );
     } else {
         runSequence(
             'clean',
-            ['index', 'styles', 'images', 'fonts', 'templates', 'lint'],
+            ['index', 'styles', 'images', 'assets', 'templates', 'lint'],
             ['watchify', 'watch', 'serve']
         );
     }
