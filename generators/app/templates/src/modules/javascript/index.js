b04cdeb93e9be3488a8a4a3e1421280d14b89d84
Julien Goux <julien.goux@live.fr>
2014-08-16 19:10:32 +0200

    fix little typo in index.js javascript version

--- generators/app/templates/src/modules/javascript/index.js
+++ generators/app/templates/src/modules/javascript/index.js
@@ -2,9 +2,8 @@
 
 //browserify-shim dependencies (can be edited in package.json)
 require('angular');
-<% if (includeUIBootstrap) { %>
-  require('angular-ui-bootstrap');<% } if (includeUIRouter) { %>
-  require('angular-ui-router');<% } else { %>
-  require('angular-route');<% } %>
+<% if (includeUIBootstrap) { %>require('angular-ui-bootstrap');<% } if (includeUIRouter) { %>
+require('angular-ui-router');<% } else { %>
+require('angular-route');<% } %>
 //app entry point
 require('./app');
