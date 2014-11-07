ffdd6c71079d7e69537a35866b7c5de2961d9212
Julien Goux <julien.goux@live.fr>
2014-11-07 16:22:51 +0100

    continue tasks index & templates on fail

--- generators/app/templates/gulp/tasks/templates/jade/index.js
+++ generators/app/templates/gulp/tasks/templates/jade/index.js
@@ -2,12 +2,18 @@
 
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var gutil = require('gulp-util');
 var inject = require('gulp-inject');
+var plumber = require('gulp-plumber');
 
 var config = require('../config').index;
 
 gulp.task('index', function () {
   return gulp.src(config.src)
+    .pipe(plumber(function(error){
+      gutil.log(gutil.colors.red(error.message));
+      this.emit('end');
+    }))
     .pipe(inject(gulp.src(config.injectSrc, {read: false}), config.inject))
     .pipe(jade(config.jade))
     .pipe(gulp.dest(config.dest));
--- generators/app/templates/gulp/tasks/templates/jade/templates.js
+++ generators/app/templates/gulp/tasks/templates/jade/templates.js
@@ -1,14 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
-var path = require('path');
 var jade = require('gulp-jade');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 var templateCache = require('gulp-angular-templatecache');
 
 var config = require('../config').templates;
 
 gulp.task('templates', function () {
   return gulp.src(config.src)
+    .pipe(plumber(function(error){
+      gutil.log(gutil.colors.red(error.message));
+      this.emit('end');
+    }))
     .pipe(jade(config.jade))
     .pipe(templateCache(config.templateCache.filename, config.templateCache.options))
     .pipe(gulp.dest(config.dest));
